fe80ac8d6908e662b8dd0932c8e27c290566157a
amineYc <amineamoune904@gmail.com>
2024-09-17 21:26:38 +0100

    commenting and fix format

--- myFunctions/bytesToAsciiMap.go
+++ myFunctions/bytesToAsciiMap.go
@@ -7,9 +7,9 @@ func BytesToAsciiMap(style []byte) map[int]string {
 	intValue := 32
 	splitedStyle := strings.Split(string(style[1:]), "\n\n")
 	//Range char by char standard to fill each ascii separatly in the map.
-		for _, v := range splitedStyle {
-			chars[intValue] = v
-			intValue++
-		}
+	for _, v := range splitedStyle {
+		chars[intValue] = v
+		intValue++
+	}
 	return chars
 }
