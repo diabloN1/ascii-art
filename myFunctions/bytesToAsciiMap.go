a8a542ea114edea1a3a2fe010734f99b4f4562d7
diabloN1 <135464742+diabloN1@users.noreply.github.com>
2024-09-19 18:38:59 +0100

    Update bytesToAsciiMap.go

--- myFunctions/bytesToAsciiMap.go
+++ myFunctions/bytesToAsciiMap.go
@@ -7,8 +7,8 @@ import (
 func BytesToAsciiMap(style []byte) map[byte]string {
 	chars := make(map[byte]string)
 	intValue := 32
-	splitedStyle := strings.Split(string(style[1:]), "\n\n")
-	//Range char by char standard to fill each ascii separatly in the map.
+	splitedStyle := strings.Split(string(style[1:len(style)-1]), "\n\n")
+	// Range char by char standard to fill each ascii separatly in the map.
 	for _, v := range splitedStyle {
 		chars[byte(intValue)] = v
 		intValue++
